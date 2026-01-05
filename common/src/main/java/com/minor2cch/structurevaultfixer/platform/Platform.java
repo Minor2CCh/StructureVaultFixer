f037f325e529576dcbe5bd023cc03ee30e68ab94
Minor2CCh <minormugen716@gmail.com>
2026-01-05 19:22:46 +0900

    Port 1.21.11.

--- common/src/main/java/com/minor2cch/structurevaultfixer/platform/Platform.java
+++ common/src/main/java/com/minor2cch/structurevaultfixer/platform/Platform.java
@@ -1,26 +1,27 @@
 package com.minor2cch.structurevaultfixer.platform;
 
-import net.minecraft.Util;
-
 import java.nio.file.Path;
 import java.util.Iterator;
 import java.util.ServiceLoader;
 
 @SuppressWarnings("unused")
 public interface Platform {
-    Platform INSTANCE = Util.make(() -> {
-        final ServiceLoader<Platform> loader = ServiceLoader.load(Platform.class);
-        final Iterator<Platform> iterator = loader.iterator();
+    Platform INSTANCE = create();
+    private static Platform create() {
+        ServiceLoader<Platform> loader = ServiceLoader.load(Platform.class);
+        Iterator<Platform> iterator = loader.iterator();
+
         if (!iterator.hasNext()) {
             throw new RuntimeException("Platform instance not found!");
-        } else {
-            final Platform platform = iterator.next();
-            if (iterator.hasNext()) {
-                throw new RuntimeException("More than one platform instance was found!");
-            }
-            return platform;
         }
-    });
+
+        Platform platform = iterator.next();
+
+        if (iterator.hasNext()) {
+            throw new RuntimeException("More than one platform instance was found!");
+        }
+        return platform;
+    }
     enum ModLoader{
         NEOFORGE,
         FABRIC
